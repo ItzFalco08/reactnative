cfea83555d2db3e1b6f1db072679eb32c5122a0d
ItzFalco08 <somadas91258@gmail.com>
2025-02-13 08:23:42 +0000

    v1

---
+++ app/(tabs)/explore.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+export default function TabTwoScreen() {
+  return (
+    <View className="py-20 px-10">
+      <Text className="text-white text-3xl font-bold">Hello Bachho</Text>
+      <Text className="text-white text-xl pt-10">Welcome to CodeWithFalco</Text>
+    </View>
+  );
+}
